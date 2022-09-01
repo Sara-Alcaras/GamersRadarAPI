f26e3d1eb94c405d9dca7c725a55a0e7affe756b
Sara-Alcaras <saraalcaras401@gmail.com>
2022-08-31 22:53:43 -0300

    adiciona requerimento para a classe de categoria

--- GamersRadarAPI/Models/Comentarios.cs
+++ GamersRadarAPI/Models/Comentarios.cs
@@ -1,16 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GamersRadarAPI.Models
 {
-    public class Categoria
+    public class Comentarios
     {
+
         // [Required] = O campo não pode ser vazio
         // [MaxLength(900)] = Max de caractere permitido
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Informe uma Categoria")]
         [MaxLength(900)]
-        public string TipoCategoria { get; set; }
+        public string Comentario { get; set; }
+        public DateTime DataComentario { get; set; }
+
+        [Required(ErrorMessage = "Informe o Id do Perfil")]
+        public int PerfilId { get; set; }
 
         [Required(ErrorMessage = "Informe o Id da Publicação")]
         public int PublicacoesId { get; set; }
